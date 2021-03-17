ccdaf256205c717163153ba4eb4c24268161f093
m-i-n-a-r <glucaconti.952@gmail.com>
2021-03-18 00:38:59 +0100

    Up to 10 dices, improvements

--- app/src/main/java/com/minar/randomix/fragments/DiceFragment.java
+++ app/src/main/java/com/minar/randomix/fragments/DiceFragment.java
@@ -19,6 +19,8 @@ import android.widget.TextView;
 import com.minar.randomix.activities.MainActivity;
 import com.minar.randomix.R;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 import java.util.Random;
 
@@ -26,9 +28,37 @@ public class DiceFragment extends androidx.fragment.app.Fragment implements OnCl
     // There's a difference in animations between the first throw and the others
     private boolean notFirstThrow = false;
     // Last result to select the correct animation
-    private int lastResult1, lastResult2, lastResult3;
-    private String chosenDrawable1, chosenDrawable2, chosenDrawable3;
-    private ImageView diceAnimation1, diceAnimation2 = null, diceAnimation3 = null;
+    private int lastResult1,
+            lastResult2,
+            lastResult3,
+            lastResult4,
+            lastResult5,
+            lastResult6,
+            lastResult7,
+            lastResult8,
+            lastResult9,
+            lastResult10;
+    private String chosenDrawable1,
+            chosenDrawable2,
+            chosenDrawable3,
+            chosenDrawable4,
+            chosenDrawable5,
+            chosenDrawable6,
+            chosenDrawable7,
+            chosenDrawable8,
+            chosenDrawable9,
+            chosenDrawable10;
+    private ImageView diceAnimation1,
+            diceAnimation2 = null,
+            diceAnimation3 = null,
+            diceAnimation4 = null,
+            diceAnimation5 = null,
+            diceAnimation6 = null,
+            diceAnimation7 = null,
+            diceAnimation8 = null,
+            diceAnimation9 = null,
+            diceAnimation10 = null,
+            vsAnimation = null;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -44,23 +74,46 @@ public class DiceFragment extends androidx.fragment.app.Fragment implements OnCl
             v.findViewById(R.id.descriptionDice).setVisibility(View.GONE);
         int diceNumber = Integer.parseInt(Objects.requireNonNull(sp.getString("dice_number", "1")));
         // Choose the correct layout. diceNumber can only be 1, 2 or 3
+        LinearLayout diceZone;
         switch (diceNumber) {
             case 1:
                 inflater.inflate(R.layout.single_dice, diceSection);
-                LinearLayout singleDiceZone = v.findViewById(R.id.diceZone);
-                singleDiceZone.setOnClickListener(this);
                 break;
             case 2:
                 inflater.inflate(R.layout.double_dice, diceSection);
-                LinearLayout doubleDiceZone = v.findViewById(R.id.diceZone);
-                doubleDiceZone.setOnClickListener(this);
                 break;
             case 3:
                 inflater.inflate(R.layout.triple_dice, diceSection);
-                LinearLayout tripleDiceZone = v.findViewById(R.id.diceZone);
-                tripleDiceZone.setOnClickListener(this);
                 break;
+            case 4:
+                inflater.inflate(R.layout.hexa_dice, diceSection);
+                break;
+            case 5:
+                inflater.inflate(R.layout.penta_dice, diceSection);
+                break;
+            case 6:
+                inflater.inflate(R.layout.esa_dice, diceSection);
+                break;
+            case 7:
+                inflater.inflate(R.layout.septa_dice, diceSection);
+                break;
+            case 8:
+                inflater.inflate(R.layout.octa_dice, diceSection);
+                break;
+            case 9:
+                inflater.inflate(R.layout.nona_dice, diceSection);
+                break;
+            case 10:
+                inflater.inflate(R.layout.deca_dice, diceSection);
+                break;
+            case 11:
+                inflater.inflate(R.layout.triple_v_triple_dice, diceSection);
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + diceNumber);
         }
+        diceZone = v.findViewById(R.id.diceZone);
+        diceZone.setOnClickListener(this);
         return v;
     }
 
@@ -71,7 +124,7 @@ public class DiceFragment extends androidx.fragment.app.Fragment implements OnCl
             final LinearLayout diceAnimation = requireView().findViewById(R.id.diceZone);
             diceAnimation.setClickable(false);
 
-            // Get the shared preferences and the desired number of dices, from 1 to 3
+            // Get the shared preferences and the desired number of dices, from 1 to 11
             SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getContext());
             final int diceNumber = Integer.parseInt(Objects.requireNonNull(sp.getString("dice_number", "1")));
 
@@ -101,49 +154,136 @@ public class DiceFragment extends androidx.fragment.app.Fragment implements OnCl
     // Run the main animation based on the result
     private void throwAndRunMainAnimation(int diceNumber) {
         // Choose a random number between 1 and 6 with equal possibilities
-        int n1, n2 = 0, n3 = 0;
         Random ran = new Random();
-        // Get 1, 2 or 3 numbers depending on the dices number
-        n1 = ran.nextInt(6) + 1;
-
-        if (diceNumber > 1) n2 = ran.nextInt(6) + 1;
-        if (diceNumber > 2) n3 = ran.nextInt(6) + 1;
+        List<Integer> diceResults = new ArrayList<>();
+        // Get up to 10 numbers depending on the dice number
+        for (int i = 0; i < diceNumber; i++) {
+            diceResults.add(ran.nextInt(6) + 1);
+        }
 
         // Check for fragment changes. If the fragment has changed, no further operations are needed
         if (!isAdded()) return;
-        diceAnimation1 = requireView().findViewById(R.id.diceButtonAnimation1);
 
+        diceAnimation1 = requireView().findViewById(R.id.diceButtonAnimation1);
         if (diceNumber > 1) diceAnimation2 = requireView().findViewById(R.id.diceButtonAnimation2);
         if (diceNumber > 2) diceAnimation3 = requireView().findViewById(R.id.diceButtonAnimation3);
+        if (diceNumber > 3) diceAnimation4 = requireView().findViewById(R.id.diceButtonAnimation4);
+        if (diceNumber > 4) diceAnimation5 = requireView().findViewById(R.id.diceButtonAnimation5);
+        if (diceNumber > 5) diceAnimation6 = requireView().findViewById(R.id.diceButtonAnimation6);
+        if (diceNumber > 6 && diceNumber < 11)
+            diceAnimation7 = requireView().findViewById(R.id.diceButtonAnimation7);
+        if (diceNumber > 7 && diceNumber < 11)
+            diceAnimation8 = requireView().findViewById(R.id.diceButtonAnimation8);
+        if (diceNumber > 8 && diceNumber < 11)
+            diceAnimation9 = requireView().findViewById(R.id.diceButtonAnimation9);
+        if (diceNumber == 10)
+            diceAnimation10 = requireView().findViewById(R.id.diceButtonAnimation10);
+        if (diceNumber == 11) vsAnimation = requireView().findViewById(R.id.diceButtonAnimationVs);
 
         // Set the drawable programmatically, depending on the dices number
-        chosenDrawable1 = "dice_" + n1 + "_vector_animation";
+        chosenDrawable1 = "dice_" + diceResults.get(0) + "_vector_animation";
         int resId1 = getResources().getIdentifier(chosenDrawable1, "drawable", "com.minar.randomix");
         diceAnimation1.setImageResource(resId1);
         // Animate the drawable
         Drawable diceDrawable1 = diceAnimation1.getDrawable();
         ((Animatable) diceDrawable1).start();
+        this.lastResult1 = diceResults.get(0);
 
         if (diceNumber > 1) {
-            chosenDrawable2 = "dice_" + n2 + "_vector_animation";
+            chosenDrawable2 = "dice_" + diceResults.get(1) + "_vector_animation";
             int resId2 = getResources().getIdentifier(chosenDrawable2, "drawable", "com.minar.randomix");
             diceAnimation2.setImageResource(resId2);
             // Animate the drawable
             Drawable diceDrawable2 = diceAnimation2.getDrawable();
             ((Animatable) diceDrawable2).start();
+            this.lastResult2 = diceResults.get(1);
         }
         if (diceNumber > 2) {
-            chosenDrawable3 = "dice_" + n3 + "_vector_animation";
+            chosenDrawable3 = "dice_" + diceResults.get(2) + "_vector_animation";
             int resId3 = getResources().getIdentifier(chosenDrawable3, "drawable", "com.minar.randomix");
             diceAnimation3.setImageResource(resId3);
             // Animate the drawable
             Drawable diceDrawable3 = diceAnimation3.getDrawable();
             ((Animatable) diceDrawable3).start();
+            this.lastResult3 = diceResults.get(2);
+        }
+        if (diceNumber > 3) {
+            chosenDrawable4 = "dice_" + diceResults.get(3) + "_vector_animation";
+            int resId4 = getResources().getIdentifier(chosenDrawable4, "drawable", "com.minar.randomix");
+            diceAnimation4.setImageResource(resId4);
+            // Animate the drawable
+            Drawable diceDrawable4 = diceAnimation4.getDrawable();
+            ((Animatable) diceDrawable4).start();
+            this.lastResult4 = diceResults.get(3);
+        }
+        if (diceNumber > 4) {
+            chosenDrawable5 = "dice_" + diceResults.get(4) + "_vector_animation";
+            int resId5 = getResources().getIdentifier(chosenDrawable5, "drawable", "com.minar.randomix");
+            diceAnimation5.setImageResource(resId5);
+            // Animate the drawable
+            Drawable diceDrawable5 = diceAnimation5.getDrawable();
+            ((Animatable) diceDrawable5).start();
+            this.lastResult5 = diceResults.get(4);
+        }
+        if (diceNumber > 5) {
+            chosenDrawable6 = "dice_" + diceResults.get(5) + "_vector_animation";
+            int resId6 = getResources().getIdentifier(chosenDrawable6, "drawable", "com.minar.randomix");
+            diceAnimation6.setImageResource(resId6);
+            // Animate the drawable
+            Drawable diceDrawable6 = diceAnimation6.getDrawable();
+            ((Animatable) diceDrawable6).start();
+            this.lastResult6 = diceResults.get(5);
+        }
+        if (diceNumber > 6 && diceNumber < 11) {
+            chosenDrawable7 = "dice_" + diceResults.get(6) + "_vector_animation";
+            int resId3 = getResources().getIdentifier(chosenDrawable7, "drawable", "com.minar.randomix");
+            diceAnimation7.setImageResource(resId3);
+            // Animate the drawable
+            Drawable diceDrawable7 = diceAnimation7.getDrawable();
+            ((Animatable) diceDrawable7).start();
+            this.lastResult7 = diceResults.get(6);
+        }
+        if (diceNumber > 7 && diceNumber < 11) {
+            chosenDrawable8 = "dice_" + diceResults.get(7) + "_vector_animation";
+            int resId8 = getResources().getIdentifier(chosenDrawable8, "drawable", "com.minar.randomix");
+            diceAnimation8.setImageResource(resId8);
+            // Animate the drawable
+            Drawable diceDrawable8 = diceAnimation8.getDrawable();
+            ((Animatable) diceDrawable8).start();
+            this.lastResult8 = diceResults.get(7);
+        }
+        if (diceNumber > 8 && diceNumber < 11) {
+            chosenDrawable9 = "dice_" + diceResults.get(8) + "_vector_animation";
+            int resId9 = getResources().getIdentifier(chosenDrawable9, "drawable", "com.minar.randomix");
+            diceAnimation9.setImageResource(resId9);
+            // Animate the drawable
+            Drawable diceDrawable9 = diceAnimation9.getDrawable();
+            ((Animatable) diceDrawable9).start();
+            this.lastResult9 = diceResults.get(8);
+        }
+        if (diceNumber == 10) {
+            chosenDrawable10 = "dice_" + diceResults.get(9) + "_vector_animation";
+            int resId3 = getResources().getIdentifier(chosenDrawable10, "drawable", "com.minar.randomix");
+            diceAnimation10.setImageResource(resId3);
+            // Animate the drawable
+            Drawable diceDrawable10 = diceAnimation10.getDrawable();
+            ((Animatable) diceDrawable10).start();
+            this.lastResult10 = diceResults.get(9);
+        }
+        if (diceNumber == 11) {
+            // Animate the drawable
+            Drawable vsDrawable = vsAnimation.getDrawable();
+            ((Animatable) vsDrawable).start();
         }
 
         // Get the text view and set its value depending on n
         final TextView textViewResult = requireView().findViewById(R.id.resultDice);
-        final String result = getString(R.string.generic_result) + " " + (n1 + n2 + n3);
+        String result;
+        if (diceNumber < 11)
+            result = getString(R.string.generic_result) + " " + (sumElements(diceResults));
+        else result = getString(R.string.generic_result) + " " +
+                (lastResult1 + lastResult2 + lastResult3) + " - " +
+                (lastResult4 + lastResult5 + lastResult6);
 
         // Create the animations
         final Animation animIn = new AlphaAnimation(1.0f, 0.0f);
@@ -153,22 +293,39 @@ public class DiceFragment extends androidx.fragment.app.Fragment implements OnCl
         animOut.setDuration(1000);
 
         // Delay the execution
+        String finalResult = result;
         requireView().postDelayed(() -> {
-            textViewResult.setText(result);
+            textViewResult.setText(finalResult);
             textViewResult.setSelected(true);
             textViewResult.startAnimation(animOut);
         }, 1500);
-        this.lastResult1 = n1;
-        this.lastResult2 = n2;
-        this.lastResult3 = n3;
+    }
+
+    // Sum an arbitrary number of results
+    private int sumElements(List<Integer> results) {
+        int sum = 0;
+        for (int i = 0; i < results.size(); i++) {
+            sum += results.get(i);
+        }
+        return sum;
     }
 
     // Run the reset animation to return to the initial state
     private void runResetAnimation(int diceNumber) {
         diceAnimation1 = requireView().findViewById(R.id.diceButtonAnimation1);
-
         if (diceNumber > 1) diceAnimation2 = requireView().findViewById(R.id.diceButtonAnimation2);
         if (diceNumber > 2) diceAnimation3 = requireView().findViewById(R.id.diceButtonAnimation3);
+        if (diceNumber > 3) diceAnimation4 = requireView().findViewById(R.id.diceButtonAnimation4);
+        if (diceNumber > 4) diceAnimation5 = requireView().findViewById(R.id.diceButtonAnimation5);
+        if (diceNumber > 5) diceAnimation6 = requireView().findViewById(R.id.diceButtonAnimation6);
+        if (diceNumber > 6 && diceNumber < 11)
+            diceAnimation7 = requireView().findViewById(R.id.diceButtonAnimation7);
+        if (diceNumber > 7 && diceNumber < 11)
+            diceAnimation8 = requireView().findViewById(R.id.diceButtonAnimation8);
+        if (diceNumber > 8 && diceNumber < 11)
+            diceAnimation9 = requireView().findViewById(R.id.diceButtonAnimation9);
+        if (diceNumber == 10)
+            diceAnimation10 = requireView().findViewById(R.id.diceButtonAnimation10);
 
         // Choose the correct drawable and run the reset animation
         chosenDrawable1 = "dice_" + this.lastResult1 + "_to_start_vector_animation";
@@ -191,6 +348,62 @@ public class DiceFragment extends androidx.fragment.app.Fragment implements OnCl
             diceAnimation3.setImageResource(resId3);
             Drawable diceDrawable3 = diceAnimation3.getDrawable();
             ((Animatable) diceDrawable3).start();
+        }
+
+        if (diceNumber > 3) {
+            chosenDrawable4 = "dice_" + this.lastResult4 + "_to_start_vector_animation";
+            int resId4 = getResources().getIdentifier(chosenDrawable4, "drawable", "com.minar.randomix");
+            diceAnimation4.setImageResource(resId4);
+            Drawable diceDrawable4 = diceAnimation4.getDrawable();
+            ((Animatable) diceDrawable4).start();
+        }
+
+        if (diceNumber > 4) {
+            chosenDrawable5 = "dice_" + this.lastResult5 + "_to_start_vector_animation";
+            int resId5 = getResources().getIdentifier(chosenDrawable5, "drawable", "com.minar.randomix");
+            diceAnimation5.setImageResource(resId5);
+            Drawable diceDrawable5 = diceAnimation5.getDrawable();
+            ((Animatable) diceDrawable5).start();
+        }
+
+        if (diceNumber > 5) {
+            chosenDrawable6 = "dice_" + this.lastResult6 + "_to_start_vector_animation";
+            int resId6 = getResources().getIdentifier(chosenDrawable6, "drawable", "com.minar.randomix");
+            diceAnimation6.setImageResource(resId6);
+            Drawable diceDrawable6 = diceAnimation6.getDrawable();
+            ((Animatable) diceDrawable6).start();
+        }
+
+        if (diceNumber > 6 && diceNumber < 11) {
+            chosenDrawable7 = "dice_" + this.lastResult7 + "_to_start_vector_animation";
+            int resId7 = getResources().getIdentifier(chosenDrawable7, "drawable", "com.minar.randomix");
+            diceAnimation7.setImageResource(resId7);
+            Drawable diceDrawable7 = diceAnimation7.getDrawable();
+            ((Animatable) diceDrawable7).start();
+        }
+
+        if (diceNumber > 7 && diceNumber < 11) {
+            chosenDrawable8 = "dice_" + this.lastResult8 + "_to_start_vector_animation";
+            int resId8 = getResources().getIdentifier(chosenDrawable8, "drawable", "com.minar.randomix");
+            diceAnimation8.setImageResource(resId8);
+            Drawable diceDrawable8 = diceAnimation8.getDrawable();
+            ((Animatable) diceDrawable8).start();
+        }
+
+        if (diceNumber > 8 && diceNumber < 11) {
+            chosenDrawable9 = "dice_" + this.lastResult9 + "_to_start_vector_animation";
+            int resId9 = getResources().getIdentifier(chosenDrawable9, "drawable", "com.minar.randomix");
+            diceAnimation9.setImageResource(resId9);
+            Drawable diceDrawable9 = diceAnimation9.getDrawable();
+            ((Animatable) diceDrawable9).start();
+        }
+
+        if (diceNumber == 10) {
+            chosenDrawable10 = "dice_" + this.lastResult10 + "_to_start_vector_animation";
+            int resId10 = getResources().getIdentifier(chosenDrawable10, "drawable", "com.minar.randomix");
+            diceAnimation10.setImageResource(resId10);
+            Drawable diceDrawable10 = diceAnimation10.getDrawable();
+            ((Animatable) diceDrawable10).start();
         }
     }
 
